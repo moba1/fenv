446d61f7da758a0f1b55430529872ed902e883c2
moba1 <moba2@protonmail.com>
2023-06-06 23:09:32 +0900

    add parser test

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
 #[derive(Debug)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
 pub struct EnvVar {
     key: String,
     value: String,
@@ -12,6 +13,7 @@ enum State {
 }
 
 #[derive(Debug)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
 pub enum ParseError {
     NotEnvVar,
 }
@@ -37,5 +39,49 @@ pub fn parse(arg: &str) -> Result<EnvVar, ParseError> {
             key,
             value: String::from_iter(character_sequence),
         }),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn parse_only_ascii_character() {
+        assert_eq!(
+            super::parse("test=1"),
+            Ok(super::EnvVar {
+                key: "test".to_string(),
+                value: "1".to_string()
+            })
+        );
+        assert_eq!(
+            super::parse("test\0\n=1\r"),
+            Ok(super::EnvVar {
+                key: "test\0\n".to_string(),
+                value: "1\r".to_string()
+            })
+        );
+        assert_eq!(
+            super::parse("test\"=1'"),
+            Ok(super::EnvVar {
+                key: "test\"".to_string(),
+                value: "1'".to_string()
+            })
+        );
+    }
+
+    #[test]
+    fn reject_non_env_var() {
+        assert_eq!(super::parse("test"), Err(super::ParseError::NotEnvVar))
+    }
+
+    #[test]
+    fn parse_non_ascii_character() {
+        assert_eq!(
+            super::parse("🚀=✨"),
+            Ok(super::EnvVar {
+                key: "🚀".to_string(),
+                value: "✨".to_string()
+            })
+        )
     }
 }
